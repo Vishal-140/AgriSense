c15c1594620ee4d627c2ea88d4bf1c8432e4cd88
Shubsriv001 <shubsriv001@gmail.com>
2025-09-29 18:55:56 +0530

    added agrinews page with complete ui, added drawer navigation and drawable designs.

--- app/src/main/java/com/example/agrisense/MainActivity.kt
+++ app/src/main/java/com/example/agrisense/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.agrisense
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val drawer_layout:DrawerLayout = findViewById(R.id.drawer_layout)
+        val drawer_button:View = findViewById(R.id.drawer_menu_icon)
+
+
+        drawer_button.setOnClickListener {
+            drawer_layout.open()
         }
+
     }
 }
---
+++ app/src/main/java/com/example/agrisense/News_page.kt
@@ -0,0 +1,16 @@
+package com.example.agrisense
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class News_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_news_page)
+
+    }
+}
