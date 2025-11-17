bf5059981acc0bd69f21892002dfea9cc924732c
Vishal Kumar Chaurasia <vkc140@gmail.com>
2025-11-17 12:08:50 +0530

    Crop Recommendation

--- app/src/main/java/com/example/agrisense/HomeActivity.kt
+++ app/src/main/java/com/example/agrisense/HomeActivity.kt
@@ -1,12 +1,21 @@
 package com.example.agrisense
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val recommendCard = findViewById<CardView>(R.id.recommendCrop)
+
+        recommendCard.setOnClickListener {
+            val intent = Intent(this, Recommendation::class.java)
+            startActivity(intent)
+        }
 
         // bottom navigation
         setupBottomNavigation(this)
